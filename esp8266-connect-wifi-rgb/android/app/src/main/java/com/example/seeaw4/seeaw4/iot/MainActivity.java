2a8285619110acd5c982880aa62313b50f3a1195
MY <715711877@qq.com>
2020-11-07 20:31:49 +0800

    update

--- esp8266-connect-wifi-rgb/android/app/src/main/java/com/example/seeaw4/seeaw4/iot/MainActivity.java
+++ esp8266-connect-wifi-rgb/android/app/src/main/java/com/example/seeaw4/seeaw4/iot/MainActivity.java
@@ -1,44 +1,150 @@
 package com.example.seeaw4.seeaw4.iot;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int r, g, b;
+    private long lastTrigger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.openBtn).setOnClickListener(new View.OnClickListener() {
+        SeekBar sb1 = findViewById(R.id.sb1);
+        SeekBar sb2 = findViewById(R.id.sb2);
+        SeekBar sb3 = findViewById(R.id.sb3);
+        sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-                open();
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                r = progress;
+                setColor();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
-        findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
+        sb2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                g = progress;
+                setColor();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        sb3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                b = progress;
+                setColor();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        Button randomBtn = findViewById(R.id.randomBtn);
+        randomBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                close();
+                Random random = new Random();
+                r = random.nextInt(256);
+                g = random.nextInt(256);
+                b = random.nextInt(256);
+                sb1.setProgress(r);
+                sb2.setProgress(g);
+                sb3.setProgress(b);
+                setColor();
+            }
+        });
+        findViewById(R.id.resetBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                r = 0;
+                g = 0;
+                b = 0;
+                sb1.setProgress(r);
+                sb2.setProgress(g);
+                sb3.setProgress(b);
+                setColor();
             }
         });
     }
 
-    private void open(){
+
+    private void setColor() {
+        Log.d("progress", r + "," + g + "," + b);
+        if (System.currentTimeMillis() - lastTrigger < 1000) {
+            return;
+        }
+        lastTrigger = System.currentTimeMillis();
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Socket socket = new Socket("192.168.1.103",1999);
+                    Socket socket = new Socket("192.168.1.103", 1999);
                     PrintStream pw = new PrintStream(socket.getOutputStream());
-                    pw.print('O');
-                    pw.flush();
-                    pw.close();
+                    String rStr = "";
+                    String gStr = "";
+                    String bStr = "";
+                    if (r < 100) {
+                        rStr += "0";
+                        if (r < 10) {
+                            rStr += "0";
+                        }
+                    }
+                    rStr += r;
+
+                    if (g < 100) {
+                        gStr += "0";
+                        if (g < 10) {
+                            gStr += "0";
+                        }
+                    }
+                    gStr += g;
+
+                    if (b < 100) {
+                        bStr += "0";
+                        if (b < 10) {
+                            bStr += "0";
+                        }
+                    }
+                    bStr += b;
+                    pw.println(rStr + "," + gStr + "," + bStr + "F");
                     socket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -48,22 +154,4 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    private void close(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Socket socket = new Socket("192.168.1.103",1999);
-                    PrintStream pw = new PrintStream(socket.getOutputStream());
-                    pw.print('C');
-                    pw.flush();
-                    pw.close();
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }).start();
-    }
 }
--- esp8266-connect-wifi-rgb/esp8266-connect-wifi-rgb.ino
+++ esp8266-connect-wifi-rgb/esp8266-connect-wifi-rgb.ino
@@ -1,6 +1,5 @@
 #include <SoftwareSerial.h>
 
-
 #define WIFI_TX       3 // 可能芯片上面标错了 这里需要根芯片的TX RX相反
 #define WIFI_RX       2
 #define rPin           A0
@@ -27,36 +26,70 @@ void setup() {
   delay(1000);
   wifi.print("AT+CIPSERVER=1,1999\r\n");
 }
+int t;
 char c;
+char buffer[64];
+int pos = 0;
 bool inProcess = false;
 int r,g,b;
+
+void readline(){
+  t = wifi.read();
+  c = (char)t;
+  if (pos >= 63){
+    for(int i = 0;i<64;i++) buffer[i] = '\0';
+    pos = 0;
+  }
+  buffer[pos++] = t;
+  if (c == 'F'){
+    Serial.println("got it");
+    resolveReuqest();
+    Serial.println(r);
+    Serial.println(g);
+    Serial.println(b);
+    setLightColor();
+    for(int i = 0;i<64;i++) buffer[i] = '\0';
+    pos = 0;
+  }
+}
+
+void setLightColor(){
+   analogWrite(rPin, r);
+   analogWrite(gPin, g);
+   analogWrite(bPin, b);
+}
+
+void resolveReuqest(){
+  char rA[4];
+  char gA[4];
+  char bA[4];
+  for(int i = 0;i<64;i++){
+    if (buffer[i] == ':'){
+      rA[0]=buffer[i+1];
+      rA[1]=buffer[i+2];
+      rA[2]=buffer[i+3];
+      rA[3]='\0';
+      r =String(rA).toInt();
+      gA[0]=buffer[i+5];
+      gA[1]=buffer[i+6];
+      gA[2]=buffer[i+7];
+      gA[3]='\0';
+      g =String(gA).toInt();
+      bA[0]=buffer[i+9];
+      bA[1]=buffer[i+10];
+      bA[2]=buffer[i+11];
+      bA[3]='\0';
+      b =String(bA).toInt();
+    }
+  }
+}
+
 void loop() {
   if(wifi.available()){
-    c= char(wifi.read());
-    Serial.println(c);
-    if (c == '+'){
-      inProcess = true;
-    }
-    if (c == 'O' && inProcess){
-      Serial.println("open");
-      inProcess = false;
-      r = random(0,256);
-      g = random(0,256);
-      b = random(0,256);
-      analogWrite(rPin, r);
-      analogWrite(gPin, g);
-      analogWrite(bPin, b);
-    }else if (c == 'C' && inProcess){
-      Serial.println("close");
-      analogWrite(rPin, 0);
-      analogWrite(gPin, 0);
-      analogWrite(bPin, 0);
-      inProcess = false;
-    }
+    readline();
   }
   
   if(Serial.available()){
     wifi.write(Serial.read());
   }
-//  delay(1000);
 }
